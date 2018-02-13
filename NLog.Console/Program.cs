621aab60dc7f8d79282b5ebbb9fd3fb6887465ee
Karthik Konkula <kkonkula1192@gmail.com>
2018-02-14 01:29:08 +0530

    Add examples section

---
+++ NLog.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLog.Console
+{
+    class Program
+    {
+        private static LogEventInfo SetEventPropeties(ILogger logger, string message, Dictionary<string, string> dictionay, LogLevel loglevel)
+        {
+            LogEventInfo theEvent = new LogEventInfo(loglevel, logger.Name, message);
+
+            foreach (KeyValuePair<string, string> item in dictionay)
+            {
+                theEvent.Properties[item.Key] = item.Value;
+            }
+
+            return theEvent;
+        }
+
+        static void Main(string[] args)
+        {
+            Logger wslogger = LogManager.GetLogger("rs");
+
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("customeventproperty", "CUSTOM_EVENT_PROPERTY");
+            MappedDiagnosticsLogicalContext.Set("customheader", "CUSTOM_HEADER");
+
+            wslogger.Log(SetEventPropeties(wslogger, "This is Trace message", dictionary, LogLevel.Trace));
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ NLog.RestApiTargetServer/Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace NLog.RestApiTargetServer.Controllers
+{
+    public class Test
+    {
+        public string Level { get; set; }
+        public string Timestamp { get; set; }
+        public string Message { get; set; }
+        public string goal { get; set; }
+    }
+
+    public class HelloController : ApiController
+    {
+        [HttpPost]
+        public string Indexer([FromBodyAttribute]Test test)
+        {
+            var body = new StreamReader(HttpContext.Current.Request.InputStream).ReadToEnd();
+            var d = Newtonsoft.Json.JsonConvert.DeserializeObject(body);
+            var headers = HttpContext.Current.Request.Headers.AllKeys.ToList();
+            var values = HttpContext.Current.Request.Headers.Get("CustomHeader");
+            return "hello world";
+        }
+    }
+}
